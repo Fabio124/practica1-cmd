176c17ee66f16c0db5f367e804303ba690cc7ca7
Fabio124 <fabiocastillo41@gmail.com>
2026-02-19 21:54:31 -0500

    Primer commit clase persona, main

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main { 
+    public static void main(String[] args) { 
+        Persona p = new Persona("Carlos", 30); 
+        p.mostrar(); 
+    } 
+} 
---
+++ Persona.java
@@ -0,0 +1,11 @@
+public class Persona { 
+    private String nombre; 
+    private int edad; 
+    public Persona(String nombre, int edad) { 
+        this.nombre = nombre; 
+        this.edad = edad; 
+    } 
+    public void mostrar() { 
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad); 
+    } 
+} 
